3b34ab846a4a6688d8d7ca5e0c86603ae062e490
Chris-Pagan <christopher.pagan@outlook.com>
2022-01-12 23:00:47 -0800

    set up basic structure of todo app:

---
+++ src/components/TodoTask.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ITask } from "../interfaces";
+
+interface Props {
+  task: ITask;
+}
+
+const TodoTask = ({ task }: Props) => {
+  return (
+    <div className="task">
+      <table className="task-table">
+        <tr>
+          <th>Task Name</th>
+          <th>Task due date</th>
+          <th>Edit Task</th>
+          <th>Mark Complete</th>
+        </tr>
+        <tr>
+            <td>{task.taskName}</td>
+            <td>{task.deadline}</td>
+            <td>edit button coming soon</td>
+            <td><button>Complete (deletes task for now)</button></td>
+        </tr>
+      </table>
+      
+    </div>
+  );
+};
+
+export default TodoTask;
